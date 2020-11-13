7aa9f6c0875af1389fb2999c72f9a269906771ab
justinjbit <justinjbittar@gmail.com>
2020-11-12 22:07:22 -0800

    Added Goal list, scraper.py completely functional

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
 A simple script that scrapes finviz.com and returns a pandas dataframe.
+
+Goal list:
+[✓] Scrape finviz.com for basic financial data
+[✓] Return a DataFrame
+[] Refactor code into functions
+[] Arrange columns properly
+[] Arrange rows properly
+[] Take in multiple tickers
+[] Gather upgrade/downgrade data
+[] Gather news data
--- scraper.py
+++ scraper.py
@@ -2,16 +2,17 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
 header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
 base_url = 'https://finviz.com/quote.ashx?t='
 
-#Grab columns (all of them are uniform, simpler to grab first)
+# Building generic index
 generic_req = requests.get('https://finviz.com/quote.ashx?t=DIS',headers=header)
 generic_soup = BeautifulSoup(generic_req.text,'html.parser')
-column_list = [column.text for column in generic_soup.findAll('td',class_='snapshot-td2-cp')]
+index_list = [index.text for index in generic_soup.findAll('td',class_='snapshot-td2-cp')]
 
-#Input validation
+# Beginning of Function
+
+# Input validation
 ticker = input('Enter your ticker symbol: ').upper()
 while not ticker.isalpha() or len(ticker) > 4:
     ticker = input('Only strings less than five characters allowed. Try again! \nEnter your ticker symbol: ').upper()
@@ -24,9 +25,19 @@ try:
 except:
     print('That ticker does not exist.')
 
+# Scraping data
 soup = BeautifulSoup(request.text,'html.parser')
-data_row = [data.text for data in soup.findAll('td',class_='snapshot-td2') ]
+data_row = [data.text for data in soup.findAll('td',class_='snapshot-td2')]
+
+# Building dict
+data_dict = dict(zip(index_list,data_row))
+
+#Temporary index (currently only taking one ticker at a time)
+temp_column = [ticker]
+# Building DataFrame
+df = pd.DataFrame.from_dict(data_dict,orient = 'index',columns =temp_column)
 
 # Test prints
-print(data_row)
-print(column_list)
+#print(data_row)
+#print(index_list)
+#print(data_dict)
