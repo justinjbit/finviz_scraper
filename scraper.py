fab27284e3d603fa52786f3fd3daf0388f0be33a
justinjbit <justinjbittar@gmail.com>
2020-11-13 21:37:05 -0800

    Added another function and fixed a bug

--- scraper.py
+++ scraper.py
@@ -2,44 +2,54 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def finviz_scraper(request,fixed_index_list):
+df = None
 
-    # Scraping data
-    soup = BeautifulSoup(request.text,'html.parser')
-    data_row = [data.text for data in soup.findAll('td',class_='snapshot-td2')]
-
-    # Fixing data rows
+def finviz_row_fixer(broken_list):
+    # Fixing data in rows
     fixed_data_row = []
     i = 0
     j = 0
     z = 0
-    while j < int(len(fixed_index_list)):
+    while j < int(len(broken_list)):
         if i == 0:
-            fixed_data_row.append(data_row[0])
+            fixed_data_row.append(broken_list[0])
             i += 6
             j += 1
         elif i < 72:
-            fixed_data_row.append(data_row[i])
+            fixed_data_row.append(broken_list[i])
             i += 6
             j += 1
         else:
             z+=1
             i = z
+    return fixed_data_row
+
+def finviz_scraper(request,index_list):
+
+    # Scraping data
+    soup = BeautifulSoup(request.text,'html.parser')
+    data_row = [data.text for data in soup.findAll('td',class_='snapshot-td2')]
+
+    fixed_data_row = finviz_row_fixer(data_row)
 
     # Building dict
-    data_dict = dict(zip(fixed_index_list,fixed_data_row))
+    data_dict = dict(zip(index_list,fixed_data_row))
 
     # Temporary column (currently only taking one ticker at a time)
     temp_column = ['ticker']
     # Building DataFrame
+    global df
     df = pd.DataFrame.from_dict(data_dict,orient = 'index',columns =temp_column)
+    
+    df.to_csv()
+        
     return df
 
 def main():
     header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
     base_url = 'https://finviz.com/quote.ashx?t='
 
-    # Building generic index
+    # Building generic rows
     generic_req = requests.get('https://finviz.com/quote.ashx?t=DIS',headers=header)
     generic_soup = BeautifulSoup(generic_req.text,'html.parser')
     index_list = [index.text for index in generic_soup.findAll('td',class_='snapshot-td2-cp')]
@@ -49,22 +59,7 @@ def main():
     index_list[26] = 'EPS growth next Y '
 
     # Fixing index list
-    fixed_index_list = []
-    i = 0
-    j = 0
-    z = 0
-    while j < int(len(index_list)):
-        if i == 0:
-            fixed_index_list.append(index_list[0])
-            i += 6
-            j += 1
-        elif i < 72:
-            fixed_index_list.append(index_list[i])
-            i += 6
-            j += 1
-        else:
-            z += 1
-            i = z
+    fixed_index_list = finviz_row_fixer(index_list)
 
     # Input validation
     ticker = input('Enter your ticker symbol: ').upper()
